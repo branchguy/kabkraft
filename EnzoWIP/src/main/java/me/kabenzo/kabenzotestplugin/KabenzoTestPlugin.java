b56cb28804e2431f48b11382ff4736643e37a622
Kabenzo <enzoschlatter@gmail.com>
2020-04-25 04:24:42 -0700

    Setting Up Test Environment

---
+++ EnzoWIP/src/main/java/me/kabenzo/kabenzotestplugin/KabenzoTestPlugin.java
@@ -0,0 +1,17 @@
+package me.kabenzo.kabenzotestplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KabenzoTestPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
